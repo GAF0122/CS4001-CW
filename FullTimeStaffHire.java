7ecf5fc4277e2b01e64bcc5e5abd7d352258e16c
GAF0122 <gaf0122@my.londonmet.ac.uk>
2025-04-26 21:45:59 +0100

    Add files via upload

---
+++ FullTimeStaffHire.java
@@ -0,0 +1,16 @@
+
+/**
+ * FullTimeStaffHire, a subclass of of StaffHire created to represent a full-time staff member and it manages a few more attributes such as salary and weekly working hours and ensures 
+ * salary can only be set before a staff member has been hired.
+ *
+ * @GabrielCoelho 
+ * @26/04/2025
+ */
+
+public class FullTimeStaffHire extends StaffHire {
+    
+    //Instance variables
+    private int salary;
+    private int weeklyHours;
+    
+}
